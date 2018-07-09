a6d63e0912ff37e4b5eea9710211bc0ff5fd8f41
afincato <work@andrefincato.info>
2018-07-09 18:37:46 +0200

    convert read dungeon.md to be dat-aware

--- gui/dungeon.js
+++ gui/dungeon.js
@@ -1,14 +1,18 @@
 const fs = require('fs')
 const datn = require('dat-node')
 const model = require('./model')
+const controller = require('./controller')
 
 function dungeon (cb) {
-  datn('./files/dungeon/downloads/' + model.dungeon_key, { key: model.dungeon_key }, function (err, dat) {
-    if (err) throw err
-    dat.joinNetwork()
+  controller.on('move', () => {
+    datn('./files/dungeon/downloads/' + model.dungeon_key, { key: model.dungeon_key }, function (err, dat) {
+      if (err) throw err
+      dat.joinNetwork()
 
-    dat.archive.readFile('/dungeon.md', function (err, file) {
-      cb(file)
+      dat.archive.readFile('/dungeon.md', function (err, file) {
+        if (err) throw err
+        cb(file)
+      })
     })
   })
 
