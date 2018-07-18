d764caf42514c742d9764c5d83ff26cb689d80d3
Marcel Goethals <marcel.goethals@gmail.com>
2018-07-18 18:42:51 +0200

    drop things

--- gui/bag.js
+++ gui/bag.js
@@ -14,19 +14,20 @@ function bag () {
 function drop (filename) {
   fs.readdir('./files/bag/', (err, files) => {
     if (files.find( f => f===filename )){
-      database.drop(cmd[1], model.dungeon_key)
-      fs.rename('./files/bag/' + filename, './files/drop/' + filename)
+      database.drop(filename, model.dungeon_key)
+      fs.rename('./files/bag/' + filename, './files/drop/' + filename, (err) => {
+          if (err) throw err
+      })
       bag()
       return false
     }
   })
 }
 
-  module.exports = { 
+  module.exports = {
     getItem: bag,
     drop: drop,
     on: (tag, callback) => {
       ev.on(tag, callback)
     }
   }
-
